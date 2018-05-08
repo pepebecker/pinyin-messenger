b56d70d8928b2a29a014cd7bf14bb8878a7e49ba
Pepe Becker <mail@pepebecker.com>
2018-05-08 14:26:09 +0100

    Test Webhook

--- index.js
+++ index.js
@@ -1,32 +1,72 @@
 'use strict'
 
 const botCore = require('pinyin-bot-core')
-const Bot = require('messenger-bot')
-const http = require('http')
-const ip = require('ip')
- 
-let bot = new Bot({
-  token: process.env.PINYIN_MESSENGER_TOKEN,
-  verify: process.env.PINYIN_MESSENGER_VERIFY,
-  app_secret: process.env.PINYIN_MESSENGER_APP_SECRET
+const bodyParser = require('body-parser')
+const express = require('express')
+
+const app = express().use(bodyParser.json())
+
+app.post('/webhook', (req, res) => {
+  // Checks this is an event from a page subscription
+  if (req.body.object === 'page') {
+    // Iterates over each entry - there may be multiple if batched
+    req.body.entry.forEach(entry => {
+      // Gets the message. entry.messaging is an array, but 
+      // will only ever contain one message, so we get index 0
+      const webhook_event = entry.messaging[0]
+      console.log(webhook_event)
+    })
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED')
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404)
+  }
 })
- 
-bot.on('error', (err) => {
-  console.log(err.message)
-})
- 
-bot.on('message', (payload, reply) => {
-  let text = payload.message.text
-  botCore.processMessage(text, (res) => {
-  	reply({text: res})
-  })
-})
- 
-const port = Number(process.env.PORT || 8080)
-http.createServer(bot.middleware()).listen(port, ip.address(), err => {
-  if (err) {
-    console.error(err)
+
+app.get('/webhook', (req, res) => {
+  const VERIFY_TOKEN = process.env.PINYIN_MESSENGER_VERIFY
+  if (!VERIFY_TOKEN) {
+    console.error('You have to export PINYIN_MESSENGER_VERIFY in your envirement variables')
     process.exit(1)
   }
-  console.log(`Server running on http://${ip.address()}:${port}`)
+
+  // Parse the query params
+  const mode = req.query['hub.mode']
+  const token = req.query['hub.verify_token']
+  const challenge = req.query['hub.challenge']
+
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED')
+      res.status(200).send(challenge)
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403)
+    }
+  }
 })
+
+// let bot = new Bot({
+//   token: process.env.PINYIN_MESSENGER_TOKEN,
+//   verify: process.env.PINYIN_MESSENGER_VERIFY,
+//   app_secret: process.env.PINYIN_MESSENGER_APP_SECRET
+// })
+ 
+// bot.on('error', (err) => {
+//   console.log(err.message)
+// })
+ 
+// bot.on('message', (payload, reply) => {
+//   let text = payload.message.text
+//   botCore.processMessage(text, (res) => {
+//   	reply({text: res})
+//   })
+// })
+ 
+const port = Number(process.env.PORT || 8080)
+app.listen(port, () => console.log(`Server running on port ${port}`))
