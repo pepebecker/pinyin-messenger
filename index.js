8b5b40e96b899e3b167d486c4b4c433c07390a4b
Pepe Becker <mail@pepebecker.com>
2018-05-08 16:17:56 +0100

    Update

--- index.js
+++ index.js
@@ -97,6 +97,10 @@ app.get('/webhook', (req, res) => {
     }
   }
 })
- 
+
+app.get('/', (req, res) => {
+  res.send('你好')
+})
+
 const port = Number(process.env.PORT || 8080)
 app.listen(port, () => console.log(`Server running on port ${port}`))
