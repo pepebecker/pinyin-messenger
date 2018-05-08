888d3e277d63a40cde6f45e134c57c5245c60298
Pepe Becker <mail@pepebecker.com>
2018-05-08 15:02:45 +0100

    Test PSID

--- index.js
+++ index.js
@@ -11,10 +11,13 @@ app.post('/webhook', (req, res) => {
   if (req.body.object === 'page') {
     // Iterates over each entry - there may be multiple if batched
     req.body.entry.forEach(entry => {
-      // Gets the message. entry.messaging is an array, but 
-      // will only ever contain one message, so we get index 0
-      const webhook_event = entry.messaging[0]
+      // Gets the body of the webhook event
+      let webhook_event = entry.messaging[0]
       console.log(webhook_event)
+
+      // Get the sender PSID
+      let sender_psid = webhook_event.sender.id
+      console.log('Sender PSID: ' + sender_psid)
     })
 
     // Returns a '200 OK' response to all requests
