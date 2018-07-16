76f94a7261db2ebecff3a45523f379200600a215
Pepe Becker <mail@pepebecker.com>
2018-07-16 03:51:28 +0200

    Update deployment server

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -25,19 +25,19 @@ module.exports = {
    */
   deploy : {
     production : {
-      user : 'pepe',
-      host : 'scw.pepebecker.com',
+      user : 'root',
+      host : 'celsian.pepebecker.com',
       ref  : 'origin/master',
       repo : 'https://github.com/pepebecker/pinyin-messenger.git',
-      path : '/home/pepe/apps/production/pinyin-messenger',
+      path : '/root/apps/production/pinyin-messenger',
       'post-deploy' : source_profile + 'npm install && pm2 reload ecosystem.config.js --env production'
     },
     dev : {
-      user : 'pepe',
-      host : 'scw.pepebecker.com',
+      user : 'root',
+      host : 'celsian.pepebecker.com',
       ref  : 'origin/master',
       repo : 'https://github.com/pepebecker/pinyin-messenger.git',
-      path : '/home/pepe/apps/development/pinyin-messenger',
+      path : '/root/apps/development/pinyin-messenger',
       'post-deploy': source_profile + 'npm install && pm2 reload ecosystem.config.js --env dev',
       env  : {
         NODE_ENV: 'dev'
